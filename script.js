a18812c7364fcf39821824826dc16935d18d3212
francesco-guttuso <francescoguttuso@gmail.com>
2026-05-08 23:27:27 +0200

    add ticket price

---
+++ script.js
@@ -0,0 +1,25 @@
+const prezzoPerKm = 0.21;
+const eta = parseInt(prompt("Inserisci la tua età:"));
+const km = parseFloat(prompt("Inserisci i chilometri da percorrere:"));
+let prezzoBiglietto = km * prezzoPerKm;
+
+if (eta < 18) {
+    prezzoBigliettoMinori = prezzoBiglietto * 0.8; //  sconto del 20% per i minorenni
+} else if (eta >= 65) {
+    prezzoBigliettoOver = prezzoBiglietto * 0.6; // sconto del 40% per gli over 65
+    prezzoBigliettoMinori = prezzoBigliettoMinori.toFixed(2);
+    prezzoBigliettoOver = prezzoBigliettoOver.toFixed(2);
+}
+
+
+prezzoBiglietto = prezzoBiglietto.toFixed(2); // arrotonda a 2 decimali
+
+
+if (eta < 18) {
+    alert("Il prezzo del biglietto per i minorenni è: " + prezzoBigliettoMinori + "€");
+} else if (eta >= 65) {
+    alert("Il prezzo del biglietto per gli over 65 è: " + prezzoBigliettoOver + "€");
+} else {
+    alert("Il prezzo del biglietto è: " + prezzoBiglietto + "€");
+}
+
