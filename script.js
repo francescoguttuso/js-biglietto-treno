42b0fee0166d1b008c52bf0fb55c0f82087a791c
francesco-guttuso <francescoguttuso@gmail.com>
2026-05-09 00:06:01 +0200

    fix ticker over

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 const prezzoPerKm = 0.21;
 const eta = parseInt(prompt("Inserisci la tua età:"));
-const km = parseInt(prompt("Inserisci i chilometri da percorrere:"));
+const km = parseFloat(prompt("Inserisci i chilometri da percorrere:"));
 let prezzoBiglietto = km * prezzoPerKm;
 
 if (eta < 18) {
     prezzoBigliettoMinori = prezzoBiglietto * 0.8; //  sconto del 20% per i minorenni
-} else if (eta >= 65) {
-    prezzoBigliettoOver = prezzoBiglietto * 0.6; // sconto del 40% per gli over 65
     prezzoBigliettoMinori = prezzoBigliettoMinori.toFixed(2);
+} else if (eta > 65) {
+    prezzoBigliettoOver = prezzoBiglietto * 0.6; // sconto del 40% per gli over 65
     prezzoBigliettoOver = prezzoBigliettoOver.toFixed(2);
 }
 
@@ -17,7 +17,7 @@ prezzoBiglietto = prezzoBiglietto.toFixed(2); // arrotonda a 2 decimali
 
 if (eta < 18) {
     alert("Il prezzo del biglietto per i minorenni è: " + prezzoBigliettoMinori + "€");
-} else if (eta >= 65) {
+} else if (eta > 65) {
     alert("Il prezzo del biglietto per gli over 65 è: " + prezzoBigliettoOver + "€");
 } else {
     alert("Il prezzo del biglietto è: " + prezzoBiglietto + "€");
